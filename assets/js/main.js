8aff52b1a31e1c5e5563068f4f406327c1680257
yousefsaqoul <76205108+yousefsaqoul@users.noreply.github.com>
2021-02-08 16:17:29 +0100

    liAdd

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,13 +16,27 @@ let changeColor = document.getElementById("navHome")
 
 function changeMe(){
     changeColor.classList.add("lila")
-    changeColor.style.backgroundColor = ("lila")
     changeColor.innerHTML = "emoH"
 
 }
+function smallButton(){
+    document.getElementById("element").classList.add("small")
+    document.getElementById("element").classList.remove("big", "middle")
+}
 
 
+function middleButton(){
+    document.getElementById("element").classList.add("middle")
+    document.getElementById("element").classList.remove("big", "small")
 
+}
+
+function bigButton(){
+    document.getElementById("element").classList.add("big")
+    document.getElementById("element").classList.remove("small", "middle")
+
+
+}
 
 
 
