18cf3d279cced4498fcb8d8400e719fb1eae9123
yousefsaqoul <76205108+yousefsaqoul@users.noreply.github.com>
2021-02-08 15:15:47 +0100

    change-background

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// let left = document.getElementById("left")
+// let center = document.getElementById("center")
+// let right = document.getElementById("right")
+// let test = document.getElementById("test")
+
+// function myRight() {
+//     test.classList.add("right")
+//     test.classList.remove("center")
+
+// }
+// function myCenter() {
+//     test.classList.add("center")
+//     test.classList.remove("right")
+
+let changeColor = document.getElementById("navHome")
+
+function changeMe(){
+    changeColor.classList.add("lila")
+    changeColor.style.backgroundColor = ("lila")
+    changeColor.innerHTML = "emoH"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
